57dd33db2ba60804278f6f3b27654bb9fe4fe42b
Vivy-NX <58148230+vivy-nx@users.noreply.github.com>
2025-10-18 00:28:02 -0400

    my extremely basic barebones headers and just about nothing else

---
+++ consumer.c
@@ -0,0 +1,11 @@
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+#include <semaphore.h>
+#include <stdio.h> // i am going to print debug comments and such for a bit
+
+int main() 
+    {
+    // to be done later
+    return 0;
+    }
---
+++ producer.c
@@ -0,0 +1,10 @@
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+#include <semaphore.h>
+
+int main() 
+    {
+    // to be done later
+    return 0;
+    }
